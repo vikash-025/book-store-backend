7f6690f192b3570a6d6b18d17de958971deacc94
vikash-025 <2020ugec025@nitjsr.ac.in>
2022-07-30 13:28:21 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+// 0p2ImpdVCX2YLD6H
+mongoose
+  .connect(
+    "mongodb+srv://admin:vikash@cluster0.tuyssg8.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
